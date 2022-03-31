e26a99ad80895dcf7f905eee7ba00688ec95f48a
LoXur-ux <starkaw@yandex.ru>
2022-03-31 19:37:07 +0300

    Practice 1 done.

--- main/src/App.vue
+++ main/src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
     <MyName firName="Данил" midName="Александрович" lstName="Панишев" class="block" />
-    <Vyatsu class="block"/>
   </div>
 </template>
 
 <script lang="ts">
 import MyName from '../../my-name/src/components/MyName.vue';
-import Vyatsu from '../../vyatsu/src/components/Vyatsu.vue';
 
 export default {
   name: 'App',
   components: {
     MyName,
-    Vyatsu,
   },
 };
 </script>
